340ccbd12c62773a49ec5b8dd3dc78e46e9cccb7
C1329250 Rafael Ferreira De Araujo <c1329250@interno.bb.com.br>
2025-02-15 13:55:50 -0300

    bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ecommerce-jps-2025-angular';
+  idButton = 1;
 
   novaFuncao() {
     console.log('Nova função');
+    this.title = 'Novo título';
   }
 }
